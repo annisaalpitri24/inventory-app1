2e3477ccbad299c833d7c3a6855a6c48e3b1b203
annisaalpitri24 <annisa62144@sma.belajar.id>
2026-06-09 09:42:22 +0700

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,31 +1,14 @@
 @extends('layouts.main')
 
 @section('content')
-
-<div class="text-center mt-5">
-
-    <h1 class="display-4">
-        Inventory App
-    </h1>
-
-    <p class="lead">
-        Selamat datang pada aplikasi inventaris sederhana Laravel.
-    </p>
-
-    <div class="mt-4">
-
-        <a href="{{ route('product') }}"
-           class="btn btn-primary btn-lg">
-            Kelola Produk
-        </a>
-
-        <a href="{{ route('categories.index') }}"
-           class="btn btn-success btn-lg">
-            Kelola Kategori
-        </a>
-
+    <div class="row justify-content-center align-items-center text-center" style="min-height: calc(100vh - 200px);">
+        <div class="col-lg-8">
+            <h1 class="display-3 fw-bold mb-3">Inventory App</h1>
+            <p class="lead text-muted mb-5">Selamat datang pada aplikasi inventaris sederhana Laravel.</p>
+            <div class="d-flex justify-content-center gap-3 flex-wrap">
+                <a href="{{ route('product') }}" class="btn btn-primary btn-lg px-5 py-3">Kelola Produk</a>
+                <a href="{{ route('categories.index') }}" class="btn btn-success btn-lg px-5 py-3">Kelola Kategori</a>
+            </div>
+        </div>
     </div>
-
-</div>
-
 @endsection
