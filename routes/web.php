332f7fccb147f88e6bf412ca53f413832ff27200
annisaalpitri24 <annisa62144@sma.belajar.id>
2026-06-09 09:42:41 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,91 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/product', [ProductController::class, 'index'])->name('product');
-Route::get('/insert', [ProductController::class, 'insert']);
-Route::get('/edit/{id}', [ProductController::class, 'edit']);
-Route::post('/update/{id}', [ProductController::class, 'update']);
-Route::get('/delete/{id}', [ProductController::class, 'delete']);
-Route::get('/create', [ProductController::class, 'create']);
-Route::post('/store', [ProductController::class, 'store']);
-
-Route::resource('categories', CategoryController::class);
+/*
+|--------------------------------------------------------------------------
+| Home
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('/home', function () {
+    return redirect()->route('home');
+});
+
+/*
+|--------------------------------------------------------------------------
+| Authentication
+|--------------------------------------------------------------------------
+*/
+
+Route::get('/login', [AuthController::class, 'loginForm'])
+    ->name('login');
+
+Route::post('/login', [AuthController::class, 'login'])
+    ->name('login.process');
+
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->name('logout');
+
+/*
+|--------------------------------------------------------------------------
+| Protected Routes
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware(['auth.manual'])->group(function () {
+
+    /*
+    |--------------------------------------------------------------------------
+    | Product - Semua User Bisa Melihat
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('/product', [ProductController::class, 'index'])
+        ->name('product');
+
+    /*
+    |--------------------------------------------------------------------------
+    | Category - Semua User Bisa Melihat
+    |--------------------------------------------------------------------------
+    */
+
+    Route::get('/categories', [CategoryController::class, 'index'])
+        ->name('categories.index');
+
+    /*
+    |--------------------------------------------------------------------------
+    | Khusus Admin
+    |--------------------------------------------------------------------------
+    */
+
+    Route::middleware(['role.admin'])->group(function () {
+
+        // Product
+        Route::get('/create', [ProductController::class, 'create'])
+            ->name('product.create');
+
+        Route::post('/store', [ProductController::class, 'store'])
+            ->name('product.store');
+
+        Route::get('/edit/{id}', [ProductController::class, 'edit'])
+            ->name('product.edit');
+
+        Route::post('/update/{id}', [ProductController::class, 'update'])
+            ->name('product.update');
+
+        Route::get('/delete/{id}', [ProductController::class, 'delete'])
+            ->name('product.delete');
+
+        // Category
+        Route::resource('categories', CategoryController::class)
+            ->except(['index']);
+    });
+});
